89248520366321cf0f119b4e4c358d35782c9165
sanket.hambir <sanket.hambir@pubmatic.com>
2021-12-23 21:36:40 +0530

    First commit SFG.

---
+++ src/main/java/pet/clinic/sfg/SfgApplication.java
@@ -0,0 +1,13 @@
+package pet.clinic.sfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgApplication.class, args);
+	}
+
+}
