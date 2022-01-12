4a3a9d633458183bc663eb96d30856c89086b5db
sanket.hambir <sanket.hambir@pubmatic.com>
2022-01-12 14:49:01 +0530

    sfg-pet-clinic: Added index controller.

---
+++ pet-clinic-web/src/main/java/pet/clinic/sg/controller/indexController.java
@@ -0,0 +1,14 @@
+package pet.clinic.sg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"/", "", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+
+}
